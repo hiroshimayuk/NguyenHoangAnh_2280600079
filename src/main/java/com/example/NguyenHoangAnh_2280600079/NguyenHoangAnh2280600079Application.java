315899f2210e260bac36b62d4575c1ea2878a729
nguyenhoanganhcntt <hoanganh140mhd@gmail.com>
2026-03-19 10:56:46 +0700

    Initial commit

---
+++ src/main/java/com/example/NguyenHoangAnh_2280600079/NguyenHoangAnh2280600079Application.java
@@ -0,0 +1,13 @@
+package com.example.NguyenHoangAnh_2280600079;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenHoangAnh2280600079Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenHoangAnh2280600079Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NguyenHoangAnh_2280600079/NguyenHoangAnh2280600079ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NguyenHoangAnh_2280600079;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenHoangAnh2280600079ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
